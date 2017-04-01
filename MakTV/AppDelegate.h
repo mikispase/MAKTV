25337bc1a8cfb1b7a7e45f1fd99ac3579bd879b6
Natalija Pavlovska <Natalija@natalijas-macbook.local>
2017-04-02 00:31:17 +0200

    Initial Commit

---
+++ MakTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MakTV
+//
+//  Created by Natalija Pavlovska on 4/2/17.
+//  Copyright © 2017 Natalija Pavlovska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MakTV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MakTV
+//
+//  Created by Natalija Pavlovska on 4/2/17.
+//  Copyright © 2017 Natalija Pavlovska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
