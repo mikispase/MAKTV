dad25734cc32cac909fb85f5bf67dea3f1aa2b79
Dimitar Spasovski <dimitar.spasovski@movic.nl>
2020-12-10 10:45:36 +0100

    change organization name / remove name of my wife - old pc

--- MakTV/main.m
+++ MakTV/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  MakTV
-//
-//  Created by Natalija Pavlovska on 4/2/17.
-//  Copyright © 2017 Natalija Pavlovska. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
